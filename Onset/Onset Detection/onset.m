2ba76d0aa9666bb63528186ade4c6ca15a322ae9
Kainan <kainan.chen.cn@gmail.com>
2015-03-28 15:06:58 +0100

    Phase part solved

--- Onset/Onset Detection/onset.m
+++ Onset/Onset Detection/onset.m
@@ -14,30 +14,29 @@ M = abs(X);                                     % Spectrum Magnitude
 %% Energy
 DSE = sum(M(:,2:end)-M(:,1:end-1),1);           % DSE
 TM = 0.3*sum(M(:,1:end-1),1);                   % Magnitude threshold
-Marke = DSE>TM;                                 % Mark
-
-
+MarkE = DSE>TM;                                 % Mark
 
 
 %% Phase
-%m = F;
-
-PN=sum(M,1);
-uX=diag(inv(diag(PN)));
-uX=(uX*ones(1,length(uX)))';
-WX=M.*uX(1:size(M,1),:);
-PhaseArray=((unwrap(angle(X(:,3:end)),pi,2)).*WX(:,3:end)-(2*unwrap(angle(X(:,2:end-1)),pi,2)).*WX(:,2:end-1)+(unwrap(angle(X(:,1:end-2)),pi,2)).*WX(:,1:end-2));
-PhaseDF=sum(PhaseArray,1);
-deltap=pi*ones(1,length(PhaseDF));
-MarkP=PhaseDF>deltap;
+WX = M./repmat(sum(M),N2,1);
+P = unwrap(angle((X)));
+PDA = P(:,3:end)-2*P(:,2:end-1)+P(:,1:end-2);
+PD = abs(sum(PDA.*WX(:,2:end-1),1));
+TP = 35*pi;
+MarkP = PD(2:end)>TP;
 
 
 %% Complex domain
+stem(MarkP)
+hold on
+stem(MarkE(2:end-1),'r')
+
+
 disc=zeros(m);
 MarkC=zeros(m);
 PhaseArray = unwrap(angle(X));
 EstimatedPhase=princarg(2*PhaseArray(:,2:end)-PhaseArray(:,1:end-1));
-EstimatedSig=M(:,1:end-1).*exp(j*EstimatedPhase);
+EstimatedSig=M(:,1:end-1).*exp(1j*EstimatedPhase);
 ComDist=sum(sqrt(real(EstimatedSig-M(:,2:end)).^2+(imag(EstimatedSig-M(:,2:end)).^2)),1);
 for i=1:m-1
     uX(:,i)=abs(X(:,i))/(sum(abs(X(:,i))));
@@ -48,7 +47,7 @@ for i=1:m-1
         PhaseArray=unwrap([angle(X(:,i-1)),angle(X(:,i))]);
         ep=princarg(2*PhaseArray(:,2)-PhaseArray(:,1));
         amp=abs(X(:,i-1));
-        estsig=amp'*exp(j*ep);
+        estsig=amp'*exp(1j*ep);
         disc(i)=sum(sqrt(real((estsig-X(:,i))).^2+(imag(estsig-X(:,i))).^2)); 
     end
         
@@ -114,3 +113,5 @@ end;
 %end
 %display(i);
 %end
+%
+%PhaseArray=((unwrap(angle(X(:,3:end)),pi,2)).*WX(:,3:end)-(2*unwrap(angle(X(:,2:end-1)),pi,2)).*WX(:,2:end-1)+(unwrap(angle(X(:,1:end-2)),pi,2)).*WX(:,1:end-2));
